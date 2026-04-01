ed299f8f7e3e43d44ffec9a8ba2ef8716564838d
Gil Voremberg <gvoremberg2@gmail.com>
2026-04-01 17:17:53 -0400

    Created a new Background scene after pressing play game and made it look like star wars text

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -12,7 +12,8 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/background.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/Levels/main.tscn")
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/options.tscn")
