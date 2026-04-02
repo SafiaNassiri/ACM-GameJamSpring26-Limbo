b8406dc9a902ca0d13eb069b4aae746c247f69a6
Gil Voremberg <gvoremberg2@gmail.com>
2026-04-01 21:21:31 -0400

    Added a button to the Background scene to go to main game

--- Scripts/UI/background.gd
+++ Scripts/UI/background.gd
@@ -13,3 +13,6 @@ func _process(delta: float) -> void:
 func _on_rich_text_label_gui_input(event: InputEvent) -> void:
 	pass # Replace with function body.
 	#Space - ui_accept
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/main.tscn")
